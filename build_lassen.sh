28df4632cf4e0120792fe132b25f364bbb6a3454
Anjiang-Wei <1020681930@qq.com>
2023-08-03 22:15:25 -0700

    lassen works

--- build_lassen.sh
+++ build_lassen.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-regent=/usr/workspace/wsb/wei8/dlb/legion/language/regent.py
+module load gcc/7.3.1
+module load cmake/3.14.5
+module load cuda/11.7.0
 
-build_option="-findex-launch 1 -fdebug 1"
+# regent=/usr/workspace/wsb/wei8/dlb/legion/language/regent.py
+regent=/usr/workspace/wsb/wei8/release_exp/legion/language/regent.py
+
+build_option="-fdebug 0"
 SAVEOBJ=1 STANDALONE=1 OBJNAME=./dlb $regent dlb.rg $build_option
