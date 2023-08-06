4619bcb4db1486fda2803135120fa75987b70a47
Anjiang-Wei <1020681930@qq.com>
2023-08-05 22:30:22 -0700

    update name

--- test/test.sh
+++ test/test.sh
@@ -6,4 +6,4 @@
 
 # export LD_LIBRARY_PATH="/scratch2/anjiang/DSLMapperExp/legion/bindings/regent:$PWD"
 
-GASNET_BACKTRACE=1 mpirun --bind-to none legion_redop_test -ll:cpu 4 -lg:prof 1  -lg:prof_logfile prof_%.gz
+GASNET_BACKTRACE=1 mpirun --bind-to none legion_tasksteal_test -ll:cpu 4 -lg:prof 1  -lg:prof_logfile prof_%.gz
